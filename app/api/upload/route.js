fcc2e52ebb5396505ce8f591840849cb21a4b056
Amar Krishna <amarkrishna25@gmail.com>
2025-12-30 10:41:33 +0530

    rename libs folder to lib

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { supabase } from "../../../libs/supabase";
+import { supabase } from "../../../lib/supabase";
 
 export const runtime = "nodejs";
 
